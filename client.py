5ed3497dc00aeac9fc11f92253d04fb1ced48385
Ankush Singh Gandhi <bhanusinghank@gmail.com>
2020-09-10 10:42:57 +0530

    added comments on client.py

--- client.py
+++ client.py
@@ -1,13 +1,19 @@
+# importing few things
 import pygame
 from network import Network
 
+# width and height for game window
 width = 500
 height = 500
+# seting up window
 win = pygame.display.set_mode((width,height))
+# seting window title to be "client"
 pygame.display.set_caption("client")
 
+# variable that stores clients no.
 clientnumber = 0
 
+# player class
 class Player():
 
     def __init__(self, x, y, width, height, color):
@@ -18,10 +24,12 @@ class Player():
         self.color = color
         self.rect = (x, y, width, height)
         self.val = 1
-
+    
+    # function to create a box on the screen
     def draw(self, win):
         pygame.draw.rect(win, self.color, self.rect)
 
+    # function to set movement of box
     def move(self):
         keys = pygame.key.get_pressed()
 
@@ -37,6 +45,7 @@ class Player():
         if keys[pygame.K_DOWN]:
             self.y += self.val
 
+        # changing box's x,y axis a box again
         self.rect = (self.x, self.y, self.width, self.height)
 
 def read_pos(str):
@@ -46,25 +55,39 @@ def read_pos(str):
 def make_pos(tup):
     return str(tup[0]) + "," + str(tup[1])
 
+# function to draw box on screen and update the screen
 def redrawwindow(win,player):
-
+    # filling the screen with white color
     win.fill((255,255,255))
+    # drawing an box on window
     player.draw(win)
+    # updating the display
     pygame.display.update()
 
+# main function
 def main():
+    # setting run variable True
     run = True
+    # creating a Network class object
     n = Network()
     startpos = read_pos(n.getpos())
-    p = Player(startpos[0],startpos[1],100,100,(0,255,0))
+    # creating 1st player
+    p1 = Player(startpos[0],startpos[1],100,100,(0,255,0))
+    # creating 2nd player
     p2 = Player(0,0,100,100,(0,255,0))
 
+    # game loop
     while run:
+        # checking for quit event
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                # setting the run variable False
                 run = False
                 pygame.quit()
+        # calling move methode from player class
         p.move()
+        # redrawing box and updating screen
         redrawwindow(win,p)
 
+# calling the main function
 main()
